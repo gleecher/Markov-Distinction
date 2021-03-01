278231c138a894a8c6c98cdffb3be67323c6ed43
Gannon Leech <gleech@bowdoin.edu>
2021-02-28 19:56:10 -0500

    updates

--- mission-1.py
+++ mission-1.py
@@ -3,6 +3,7 @@ Gannon Leech
 02.28.21
 CSCI 3725 - Computational Creativity
 Mission 3: A Markov Distinction
+Title: 2304 Shades of RGB
 
 Description: When run, this file creates and displayed piece of artwork using 
 a markov chain. The image should look like a bunch of little squares in a row, 
@@ -37,25 +38,33 @@ class MarkovArtist:
         """
         Decides what the next value for red should be based on the current val
         """        
-        self.redval = np.random.choice(range(0,255), p = self.transition_matrix[self.redval])
+        self.redval = np.random.choice(range(0,255), \
+                                       p = self.transition_matrix[self.redval])
         
     def calc_new_green(self):
         """
         Decides what the next value for green should be based on the current val
         """          
-        self.greenval = np.random.choice(range(0,255), p = self.transition_matrix[self.greenval])
+        self.greenval = \
+            np.random.choice(range(0,255), \
+            p = self.transition_matrix[self.greenval])
         
     def calc_new_blue(self):
         """
         Decides what the next value for blue should be based on the current val
         """          
-        self.blueval = np.random.choice(range(0,255), p = self.transition_matrix[self.blueval])
+        self.blueval = np.random.choice(range(0,255), \
+                                    p = self.transition_matrix[self.blueval])
 
 
     def draw_image(self, width, height, offset):
         """
         Draws and displays the image of rows of squares filled by colors 
         selected through a markov process
+        
+            width (int) the width of the final image
+            height (int) the height of the final image
+            offset (int) the length and width of the squares
         """          
 
         img = Image.new("RGB", (width, height)) 
@@ -63,12 +72,16 @@ class MarkovArtist:
         for i in range(offset, height - offset, offset): 
 
             for j in range(offset, width - offset, offset):
+                
+                #Updates the colors based on their current values
                 self.calc_new_red()
                 self.calc_new_green()
                 self.calc_new_blue()
                 
                 color = (self.redval, self.greenval, self.blueval)
                 
+                #draws the rectangle in the spot based on the two for loops and
+                #the current color
                 shape = [(i, j), ( i + offset, j + offset)] 
                 img1 = ImageDraw.Draw(img)   
                 img1.rectangle(shape, fill = color)            
@@ -80,7 +93,9 @@ class MarkovArtist:
 def main():
     val = int(input("Please enter an integer value for how much randomness \
 will be in the system (1 = very random, 5 and above = less random): "))
+    
     transition_matrix = make_transition_matrix(val)
+    
     art_maker = MarkovArtist(transition_matrix)
     art_maker.draw_image(500, 500, 10)
    
